106f7c4e450e779ed006d490dac4ca3f3d52387a
JVC0 <86218971+JVC0@users.noreply.github.com>
2024-01-23 10:52:18 +0000

    ..

--- Scripts/ Notas de clase/Bucles.sh
+++ Scripts/ Notas de clase/Bucles.sh
@@ -83,3 +83,43 @@ for elem in $(date)
 do
 	echo "Elemento: $elem"
 done
+
+i=0
+while [ "$i" -lt 10 ]
+do
+	echo "El valor de i es $i"
+	#i=$((i+1))
+	((i++))
+done
+
+i=0
+
+# while [[  ]]
+# while ((i < 10))
+
+
+i=0
+until [ "$i" -gt 10 ]
+do
+	echo "El valor de i es $i"
+	#i=$((i+1))
+	((i++))
+done
+
+
+# Ejemplo de uso del break y continue (EVITARLO SIEMPRE QUE SEA POSIBLE!!)
+while true 
+do
+        read -p "Dime un valor mayor entre 10 y 20: " val
+        if [ "$val" -ge 10 ] && [ "$val" -le 20 ]
+	then
+                echo "Correcto"
+                break
+        else
+                echo "Noooo!!!"
+                continue
+        fi
+        echo "Este mensaje NUNCA se imprimirá"
+done
+
+
